bfcf2a9d38fcdf8b065dcf89d2b60106dcf441f3
Fernando Oliveira <fernandoaju78@gmail.com>
2022-04-26 21:14:24 -0300

    a12 blobs and kernel

--- twrp_cheeseburger_dumpling.mk
+++ twrp_cheeseburger_dumpling.mk
@@ -29,9 +29,12 @@ PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
-
 # Set by init module based on ro.boot.rf_version argument from bootloader
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.device ro.product.model ro.build.product 
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8998 \
+    ro.hardware.gatekeeper=msm8998
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cheeseburger_dumpling
