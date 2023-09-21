8c79f13cc9f6aa26cddcd6ef0895548939c65248
CaptainThrowback <captainthrowback@hotmail.com>
2023-09-21 16:17:28 -0300

    op5_t: Boardconfig clean - libxml2 is built by default - define TARGET_RECOVERY_FSTAB
    Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,16 +125,15 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
 TARGET_RECOVERY_DEVICE_MODULES += \
 	libion \
-	libxml2 \
 	vendor.display.config@1.0 \
 	vendor.display.config@2.0
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
 	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
 	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
 
